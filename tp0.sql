e196386f1db172a542dbb668e1090bc944888ac1
Liz <lilita.moreno@hotmail.com>
2018-08-16 17:10:29 -0300

    add tables

---
+++ tp0.sql
@@ -0,0 +1,41 @@
+drop database if exists paises;
+create database paises;
+
+\c paises
+
+create table pais(
+	pais_id integer,
+	nombre	 varchar(64),
+	independencia varchar(64),
+	continente_id integer,
+	forma_gobierno	varchar(64),
+	poblacion integer
+);
+
+create table frontera(
+	pais_id1 integer,
+	pais_id2 integer,
+	extension_km float
+);
+
+
+create table censo(
+	pais_id integer,
+	 anio integer,
+	poblacion integer
+);
+
+
+create table continente(
+	continente_id integer,
+	nombre integer
+);
+
+
+
+alter table pais   add constraint pais_pk    primary key (pais_id);
+
+
+alter table tabla add constraint tablaa_fk0 foreign key (algo)  references tabla  (algo);
+
+
