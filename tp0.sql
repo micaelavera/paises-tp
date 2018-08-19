d9b696615a78dca1aa04ce1090da847ed877d803
Liz <lilita.moreno@hotmail.com>
2018-08-19 01:11:52 -0300

    update

--- tp0.sql
+++ tp0.sql
@@ -36,12 +36,8 @@ create table continente(
 alter table pais add constraint pais_pk primary key (pais_id);
 alter table continente add constraint continente_pk primary key(continente_id);
 
-<<<<<<< HEAD
+
 alter table pais  add constraint pais_pk    primary key (pais_id);
-=======
->>>>>>> 895447a14863ea1d45fce257f0b4091a70dbbf4c
 
 --FOREIGN KEYS
 alter table pais add constraint pais_fk0 foreign key (continente_id)  references continente (continente_id);
-
-
