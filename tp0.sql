0eab2c330aa297f9773b9851befcaf92572de288
Liz <lilita.moreno@hotmail.com>
2018-08-23 16:53:10 -0300

    actualizar

--- tp0.sql
+++ tp0.sql
@@ -33,20 +33,27 @@ create table continente(
 
 
 -- PRIMARY KEYS
-alter table pais add constraint pais_pk primary key (pais_id);
-alter table continente add constraint continente_pk primary key(continente_id);
+alter table pais	 add constraint pais_pk primary key (pais_id);
+alter table continente 	 add constraint continente_pk primary key(continente_id);
+alter table censo 	 add constraint censo_pk primary key(poblacion);
+
+alter table frontera	 add constraint frontera_pk primary key(pais_id1, pais_id2);
+
+
 
 --FOREIGN KEYS
---alter table pais add constraint pais_fk0 foreign key (continente_id)  references continente (continente_id);
+alter table pais add constraint pais_fk foreign key (continente_id)  references continente (continente_id);
 
 
 --LEEMOS LOS CSV
-copy pais from '/home/lilo/Moreno-Vera-tp0/Copia de Datos_paises - Pais.csv' using delimiters ',' csv header;
+copy continente from '/home/liz/Moreno-Vera-tp0/Datos_paises - Mica_Liz - Continente.csv' using delimiters ',' csv header;
 
-copy frontera from '/home/lilo/Moreno-Vera-tp0/Copia de Datos_paises - Frontera.csv' using delimiters ',' csv header;
+copy pais from '/home/liz/Moreno-Vera-tp0/Datos_paises - Mica_Liz - Pais.csv' using delimiters ',' csv header;
 
-copy censo from '/home/lilo/Moreno-Vera-tp0/Copia de Datos_paises - Censo.csv' using delimiters ',' csv header;
+copy frontera from '/home/liz/Moreno-Vera-tp0/Datos_paises - Mica_Liz - Frontera.csv' using delimiters ',' csv header;
 
-copy continente from '/home/lilo/Moreno-Vera-tp0/Copia de Datos_paises - Continente.csv' using delimiters ',' csv header;
+copy censo from '/home/liz/Moreno-Vera-tp0/Datos_paises - Mica_Liz - Censo.csv' using delimiters ',' csv header;
+
+
 
 --modificar el path , y que solo tome la carpeta y no "home/lilo etc etc
