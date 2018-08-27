788db7d23e6b52a3cc9390826e127ebdc33515e8
Micaela <micavera2010@hotmail.com>
2018-08-26 22:22:00 -0300

    ahora se borra las tablas antes de insertarlas

--- tp0.sql
+++ tp0.sql
@@ -3,6 +3,7 @@ create database paises;
 
 \c paises
 
+drop table if exists pais cascade;
 create table pais(
 	pais_id integer,
 	nombre	 varchar(64),
@@ -12,20 +13,21 @@ create table pais(
 	poblacion integer
 );
 
+drop table if exists frontera cascade;
 create table frontera(
 	pais_id1 integer,
 	pais_id2 integer,
 	extension_km float
 );
 
-
+drop table if exists censo cascade;
 create table censo(
 	pais_id integer,
 	anio integer,
 	poblacion integer
 );
 
-
+drop table if exists continente cascade;
 create table continente(
 	continente_id integer,
 	nombre varchar(64)
@@ -35,6 +37,7 @@ create table continente(
 -- PRIMARY KEYS
 alter table pais	 add constraint pais_pk primary key (pais_id);
 alter table continente 	 add constraint continente_pk primary key(continente_id);
+-- cambiar el pk de censo
 alter table censo 	 add constraint censo_pk primary key(poblacion);
 
 alter table frontera	 add constraint frontera_pk primary key(pais_id1, pais_id2);
@@ -46,14 +49,10 @@ alter table pais add constraint pais_fk foreign key (continente_id)  references 
 
 
 --LEEMOS LOS CSV
-copy continente from '/home/liz/Moreno-Vera-tp0/Datos_paises - Mica_Liz - Continente.csv' using delimiters ',' csv header;
+copy continente from '/home/lilo/Moreno-Vera-tp0/Datos_paises - Mica_Liz - Continente.csv' using delimiters ',' csv header;
 
-copy pais from '/home/liz/Moreno-Vera-tp0/Datos_paises - Mica_Liz - Pais.csv' using delimiters ',' csv header;
+copy pais from '/home/lilo/Moreno-Vera-tp0/Datos_paises - Mica_Liz - Pais.csv' using delimiters ',' csv header;
 
-copy frontera from '/home/liz/Moreno-Vera-tp0/Datos_paises - Mica_Liz - Frontera.csv' using delimiters ',' csv header;
+copy frontera from '/home/lilo/Moreno-Vera-tp0/Datos_paises - Mica_Liz - Frontera.csv' using delimiters ',' csv header;
 
-copy censo from '/home/liz/Moreno-Vera-tp0/Datos_paises - Mica_Liz - Censo.csv' using delimiters ',' csv header;
-
-
-
---modificar el path , y que solo tome la carpeta y no "home/lilo etc etc
+copy censo from '/home/lilo/Moreno-Vera-tp0/Datos_paises - Mica_Liz - Censo.csv' using delimiters ',' csv header;
