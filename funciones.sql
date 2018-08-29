2f3a3a01b9bc13a500610d635628b681ea76fc79
Liz <lilita.moreno@hotmail.com>
2018-08-28 21:52:41 -0300

    update

--- funciones.sql
+++ funciones.sql
@@ -10,15 +10,13 @@ declare
     anteultimo_censo record;
     coeficiente float;
     anios_entre_censos integer;
-
-
 begin
     select * into ultimo_censo from censo c1 where c1.pais_id= pais_id_param order by anio desc limit 1;
     select * into anteultimo_censo from censo c2 where c2.pais_id= pais_id_param order by anio desc limit 1 offset 1;
-    resul = (ultimo_censo.anio/anteultimo_censo.anio);
-    anios_entre_censos = (ultimo_censo - anteultimo_censo);
-    coeficiente= power(resul,(1/anios_entre_censos))-10
-
+    resul = truncate((ultimo_censo.poblacion/anteultimo_censo.poblacion),2);
+    anios_entre_censos = (ultimo_censo.anio - anteultimo_censo.anio);
+    coeficiente= power(resul,(1/anios_entre_censos))-1; 
+    return coeficiente;
 end;
 $$language plpgsql;
 
@@ -39,7 +37,7 @@ disponible, y la población estimada actual.
 /*
 6) Crear una función get_pop_by_continent() que devuelva la cantidad de población actual estimada de un continente.
 */
-
+/*
 create or replace function get_pop_by_continent(cont_id) returns integer as $$
 declare
 	poblaciones record;
@@ -49,7 +47,7 @@ begin
 	return poblaciones.suma_poblacion;
 end;
 $$language plpgsql;
-
+*/
 
 /*
 9) Hacer un query que obtenga la lista de pares de paises que tienen limites entre si ordenado por extension de la frontera.
