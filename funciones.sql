43890656a0bfecd8117ff1f7d7f0b6dc57496128
demo <demo@g3w2-demo.(none)>
2018-09-05 15:54:21 -0300

    updatee 9)

--- funciones.sql
+++ funciones.sql
@@ -52,29 +52,21 @@ $$language plpgsql;
 9) Hacer un query que obtenga la lista de pares de paises que tienen limites entre si ordenado por extension de la frontera.
 */
 
-
-
-
---sale cualquier cosa 
---select pais1.nombre as pais_1,pais2.nombre as pais_2, f.extension_km as extension from pais pais1, pais pais2, frontera f order by f.extension_km;
---sale bien
---select p1.nombre as pais_1,p2.nombre as pais_2, f.extension_km as extension from pais p1, pais p2 inner join frontera f on p1.pais_id,p2.pais_id order by f.extension_km;
-
-
+--select p1.nombre as pais_1,p2.nombre as pais_2, f.extension_km as extension from pais p1, pais p2, frontera f where f.pais_id1= p1.pais_id and f.pais_id2=p2.pais_id order by f.extension_km;
 
 
 /*
 10) Hacer un query que retorne todos los grupos de 3 paises que limitan entre sí. Hacer este query de 2 formas: una con un producto cartesiano de 3 veces la tabla paises y luego verificar que los pares limiten entre sí mediante subqueries correlacionados con la tabla de fronteras. La otra forma es haciendo un join triple de la tabla fronteras
 */
 
-/* select p1.nombre as pais_1, p2.nombre as pais_2,p3.nombre as pais_3 
+select p1.nombre as pais_1, p2.nombre as pais_2,p3.nombre as pais_3 
 from pais p1, pais p2, pais p3, frontera f1, frontera f2, frontera f3
- where f1.pais_id1=f3.pais_id2 
+where f1.pais_id1=f3.pais_id2 
 and f1.pais_id2= f2.pais_id1 
 and f2.pais_id2= f3.pais_id1 
 and p1.pais_id =f1.pais_id1 
 and p2.pais_id= f2.pais_id1 
-and p3.pais_id=f3.pais_id2;*/
+and p3.pais_id=f3.pais_id2;
 
 /*
 11) crear un algún indice que acelere la búsqueda de 10
