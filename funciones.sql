7ef67829ef439770e642a4297978793a588aa10f
Micaela Vera <micavera2010@hotmail.com>
2018-09-06 15:53:13 -0300

    modificacion de index

--- funciones.sql
+++ funciones.sql
@@ -2,7 +2,6 @@
 3)Crear una función llamada get_pop_variation_rate(pais_id) que retorne el coeficiente con el que varía anualmente la población en dicho país, tomado en base a los 2 últimos censos.
 Es decir esta función debe retornar un número real. por ejemplo si la población crece a un 12% anual, debería retornar 1.12
 */
-
 create or replace function get_pop_variation_rate(pais_id_param integer) returns float as $$
 declare 
     resul float;
@@ -26,7 +25,7 @@ disponible, y la población estimada actual.
 */
 
 create or replace view vista_poblaciones as(
-		select distinct p.nombre,ultimo_censo.anio_censo,censo_poblacion.poblacion,date_part('year',now()) as anio_actual,round(censo_poblacion.poblacion*(power(get_pop_variation_rate(p.pais_id),date_part('year',now())-censo_poblacion.anio))) as poblacion_total  from (select c.pais_id, max(anio) as anio_censo from censo c group by c.pais_id) as ultimo_censo inner join (select c1.pais_id,c1.anio,c1.poblacion from censo c1) as censo_poblacion on (ultimo_censo.pais_id=censo_poblacion.pais_id and ultimo_censo.anio_censo=censo_poblacion.anio) inner join pais p on(p.pais_id=ultimo_censo.pais_id)
+	select distinct p.nombre,ultimo_censo.anio_censo,censo_poblacion.poblacion,date_part('year',now()) as anio_actual,round(censo_poblacion.poblacion*(power(get_pop_variation_rate(p.pais_id),date_part('year',now())-censo_poblacion.anio))) as poblacion_total  from (select c.pais_id, max(anio) as anio_censo from censo c group by c.pais_id) as ultimo_censo inner join (select c1.pais_id,c1.anio,c1.poblacion from censo c1) as censo_poblacion on (ultimo_censo.pais_id=censo_poblacion.pais_id and ultimo_censo.anio_censo=censo_poblacion.anio) inner join pais p on(p.pais_id=ultimo_censo.pais_id)
 );
 
 /*
@@ -50,12 +49,13 @@ $$language plpgsql;
 
 /*
 9) Hacer un query que obtenga la lista de pares de paises que tienen limites entre si ordenado por extension de la frontera.
-*/\q
+*/
 
 select p1.nombre as pais_1,p2.nombre as pais_2, f.extension_km as extension 
-from pais p1, pais p2, frontera f 
-where f.pais_id1= p1.pais_id and f.pais_id2=p2.pais_id order by f.extension_km;
-
+	from pais p1, pais p2, frontera f 
+		where f.pais_id1= p1.pais_id 
+			and f.pais_id2=p2.pais_id 
+				order by f.extension_km;
 
 /*
 10) Hacer un query que retorne todos los grupos de 3 paises que limitan entre sí. Hacer este query de 2 formas: una con un producto cartesiano de 3 veces la tabla paises y luego verificar que los pares limiten entre sí mediante subqueries correlacionados con la tabla de fronteras. */
@@ -70,7 +70,7 @@ and  p2.pais_id in (--p2 con p3
 			 where p2.pais_id=f2.pais_id1 and p3.pais_id=f2.pais_id2)
 and p1.pais_id in (--p1 con p3
 			 select  f3.pais_id1 from frontera f3 
-			 where p1.pais_id=f3.pais_id1 and p3.pais_id =f3.pais_id2)
+			 where p1.pais_id=f3.pais_id1 and p3.pais_id =f3.pais_id2);
 
 ---- version Mica
 select distinct p1.nombre as pais1,p2.nombre as pais2,p3.nombre as pais3 
@@ -85,16 +85,16 @@ La otra forma es haciendo un join triple de la tabla fronteras
 */
 
 select p1.nombre as pais_1, p2.nombre as pais_2,p3.nombre as pais_3 
-from pais p1, pais p2, pais p3, frontera f1, frontera f2, frontera f3
- where (f1.pais_id1 = f3.pais_id1 or f3.pais_id1 = f1.pais_id1)
- and (f2.pais_id1 = f1.pais_id2   or f1.pais_id2 = f2.pais_id1)
- and (f3.pais_id2 = f2.pais_id2   or f2.pais_id2 = f3.pais_id2)
- and p1.pais_id =f1.pais_id1  
- and p2.pais_id= f2.pais_id1 
- and p3.pais_id=f3.pais_id2;
+	from pais p1, pais p2, pais p3, frontera f1, frontera f2, frontera f3
+ 		where (f1.pais_id1 = f3.pais_id1 or f3.pais_id1 = f1.pais_id1)
+ 		and (f2.pais_id1 = f1.pais_id2   or f1.pais_id2 = f2.pais_id1)
+ 		and (f3.pais_id2 = f2.pais_id2   or f2.pais_id2 = f3.pais_id2)
+ 		and p1.pais_id =f1.pais_id1  
+ 		and p2.pais_id= f2.pais_id1 
+ 		and p3.pais_id=f3.pais_id2;
 
 
 /*
 11) crear un algún indice que acelere la búsqueda de 10
 */
-create  or replace index frontera_indice on frontera( pais_id1,pais_id2);
+create index frontera_indice on frontera(pais_id1,pais_id2);
